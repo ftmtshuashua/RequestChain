90a797859bc785063ebcf2271f99855301724aab
LiFuPing <ftmtshuashua@users.noreply.github.com>
2019-06-27 20:24:07 +0800

    log

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
 # RequestChain
+基于EventChain的Android请求事件链，请求通过Retrofit+RxJava+OkHttp实现
+
+
+>链式请求
+
+>并发请求
+
+>事件链监听
+
+
+
+## 使用
+使用方式参考：配合Java8的lambda语法使用逻辑更清晰
+![MainActivity](https://github.com/ftmtshuashua/RequestChain/blob/master/app/src/main/java/support/lfp/requestchain/MainActivity.java)
+
+
+## 配置依赖
+
+在项目的build.gradle中添加
+```
+allprojects {
+    repositories {
+        maven { url 'https://www.jitpack.io' }
+    }
+}
+```
+在Model的build.gradle中添加 [![](https://jitpack.io/v/ftmtshuashua/RequestChain.svg)](https://jitpack.io/#ftmtshuashua/RequestChain)
+```
+dependencies {
+    implementation 'com.github.ftmtshuashua:RequestChain:version'
+}
+```
+Java8 配置
+```
+再Model(app)下的build.gradle中配置
+android{
+  compileOptions {
+        sourceCompatibility JavaVersion.VERSION_1_8
+        targetCompatibility JavaVersion.VERSION_1_8
+    }
+ }
+```
+
+
+## LICENSE
+
+```
+Copyright (c) 2018-present, RequestChain Contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+```
+
+
+
+
--- app/src/main/java/support/lfp/requestchain/MainActivity.java
+++ app/src/main/java/support/lfp/requestchain/MainActivity.java
@@ -22,7 +22,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mTV_Text = findViewById(R.id.view_Text);
 
         getWeatherList();
@@ -31,11 +30,33 @@ public class MainActivity extends AppCompatActivity {
     /*获得气象列表数据*/
     void getWeatherList() {
         new RequestEvent<>(ApiManager.getApi().getWeatherList(), getLifecycle())
-                .setDebugRequestDelay(1000)
-                .addOnRequestListener((OnRequestSucceedListener<ModelWeatherList>) s -> mTV_Text.setText(new Gson().toJson(s)))
-                .addOnEventListener(new OnRequestShowDialog(this))
+                .setDebugRequestDelay(1000) // 延时1000毫秒之后发起该请求
+                .addOnRequestListener((OnRequestSucceedListener<ModelWeatherList>) s -> mTV_Text.setText(new Gson().toJson(s))) //请求成功监听
+                .addOnEventListener(new OnRequestShowDialog(this)) //全局事件监听
                 .start();
     }
 
+    void 链式请求() {
+        RequestEvent.create(
+                new RequestEvent<>(ApiManager.getApi().getWeatherList(), getLifecycle())
+                        .addOnRequestListener((OnRequestSucceedListener<ModelWeatherList>) s -> mTV_Text.setText(new Gson().toJson(s)))
+                        .chain(new RequestEvent<>(ApiManager.getApi().getWeatherList(), getLifecycle())
+                                .addOnRequestListener((OnRequestSucceedListener<ModelWeatherList>) s -> mTV_Text.setText(new Gson().toJson(s)))
+                              )
+                           )
+                .addOnEventListener(new OnRequestShowDialog(this)) //全局事件监听
+                .start();
+    }
 
+    void 并发请求() {
+        RequestEvent.create(
+                new RequestEvent<>(ApiManager.getApi().getWeatherList(), getLifecycle())
+                        .addOnRequestListener((OnRequestSucceedListener<ModelWeatherList>) s -> mTV_Text.setText(new Gson().toJson(s)))
+                ,
+                new RequestEvent<>(ApiManager.getApi().getWeatherList(), getLifecycle())
+                        .addOnRequestListener((OnRequestSucceedListener<ModelWeatherList>) s -> mTV_Text.setText(new Gson().toJson(s)))
+                           )
+                .addOnEventListener(new OnRequestShowDialog(this)) //全局事件监听
+                .start();
+    }
 }
