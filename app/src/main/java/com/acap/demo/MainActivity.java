2d0078dd1efe38115da97f7102bfea8e478e2b87
LiFuPing <LiFuPing@email.com>
2021-10-15 14:51:07 +0800

    '-'

--- app/src/main/java/com/acap/demo/MainActivity.java
+++ app/src/main/java/com/acap/demo/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends Activity {
     }
 
     private void run() {
-//        MyApiService.getMan()
-//                .start();
+        MyApiService.getMan()
+                .start();
     }
 }
